a975352ae1233e14f883399b77b3f05d65181a99
nickschillaci1 <nickschillaci1@gmail.com>
2016-10-17 23:15:54 -0400

    initial webview instance

--- app/src/main/java/mordor/us/acm/MainActivity.java
+++ app/src/main/java/mordor/us/acm/MainActivity.java
@@ -2,12 +2,36 @@ package mordor.us.acm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String ACM_ATTENDANCE_URL = "https://acm-attendance.firebaseapp.com/";
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initWebView();
+    }
+
+    private void initWebView() {
+        webView = (WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(ACM_ATTENDANCE_URL); //initial page load
+
+        //client overrides loadUrl after initial page load so new pages load in app, not  browser
+        class MyWebViewClient extends WebViewClient {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        }
+        webView.setWebViewClient(new MyWebViewClient());
+        return;
     }
 }
