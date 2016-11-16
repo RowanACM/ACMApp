0ba7540c8a96c6a3f7200170691058a9f1b92e4d
tylercarberry <tylercarberry@gmail.com>
2016-11-16 17:06:39 -0500

    Remember info between sessions
    After you sign in for the first time, it will remember your name and email

--- app/src/main/java/mordor/us/acm/AttendanceActivity.java
+++ app/src/main/java/mordor/us/acm/AttendanceActivity.java
@@ -1,5 +1,7 @@
 package mordor.us.acm;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsIntent;
@@ -7,6 +9,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -40,7 +44,54 @@ public class AttendanceActivity extends AppCompatActivity {
                 openUrl(generateAttendanceUrl());
             }
         });
+
+        EditText nameEditText = (EditText) findViewById(R.id.name_exit_text);
+        nameEditText.setText(readFromSharedPreferenceString("name"));
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                SharedPreferences prefs = AttendanceActivity.this.getSharedPreferences(
+                        "mordor.us.acm", Context.MODE_PRIVATE);
+
+                prefs.edit().putString("name", charSequence.toString()).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+
+        EditText emailEditText = (EditText) findViewById(R.id.email_edit_text);
+        emailEditText.setText(readFromSharedPreferenceString("email"));
+        emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                SharedPreferences prefs = AttendanceActivity.this.getSharedPreferences(
+                        "mordor.us.acm", Context.MODE_PRIVATE);
+
+                prefs.edit().putString("email", charSequence.toString()).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
+
+    private String readFromSharedPreferenceString(String key) {
+        SharedPreferences prefs = AttendanceActivity.this.getSharedPreferences(
+                "mordor.us.acm", Context.MODE_PRIVATE);
+
+        return prefs.getString(key, ""); // the default value is an empty string
+    }
+
 
     private String generateAttendanceUrl() {
         String name = ((EditText) findViewById(R.id.name_exit_text)).getText().toString();
