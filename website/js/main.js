c28130ca84b3344a9cdad86474f2f763999c9e99
chris pryer <christophpryer@gmail.com>
2016-11-17 10:29:48 -0500

    attempted to verify users

--- website/js/main.js
+++ website/js/main.js
@@ -42,6 +42,7 @@ initApp = function() {
         document.getElementById('sign-in-status').textContent = 'Signed in';
         document.getElementById('sign-in').textContent = 'Sign out';
         document.getElementById('account-details').textContent = JSON.stringify({
+
           displayName: displayName,
           email: email,
           emailVerified: emailVerified,
@@ -51,11 +52,65 @@ initApp = function() {
           providerData: providerData
         }, null, '  ');
       });
+      /*
+      // Nav bar
+      document.getElementById('nav').innerHTML =
+            '<div class="container-fluid">' +
+            '<!-- Brand and toggle get grouped for better mobile display -->' +
+            '<div class="navbar-header page-scroll">' +
+            '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">' +
+            '<span class="sr-only">Toggle navigation</span>' +
+            'Menu <i class="fa fa-bars"></i>' +
+            '</button>' +
+            '<a class="navbar-brand" href="index.html">Rowan ACM</a>' +
+            '</div>' +
+
+            '<!-- Collect the nav links, forms, and other content for toggling -->' +
+            '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+            '<ul class="nav navbar-nav navbar-right">' +
+            '<li>' +
+              '<a href="index.html">Home</a>' +
+            '</li>' +
+            '</ul>' +
+            '</div>' +
+            '<!-- /.navbar-collapse -->' +
+            '</div>' +
+            '<!-- /.container -->');*/
+
     } else {
       // User is signed out.
       document.getElementById('sign-in-status').textContent = 'Signed out';
       document.getElementById('sign-in').textContent = 'Sign in';
       document.getElementById('account-details').textContent = 'null';
+      /*
+      document.getElementById('nav').innerHTML =
+          '<div class="container-fluid">' +
+          '<!-- Brand and toggle get grouped for better mobile display -->' +
+          '<div class="navbar-header page-scroll">' +
+          '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">' +
+          '<span class="sr-only">Toggle navigation</span>' +
+          'Menu <i class="fa fa-bars"></i>' +
+          '</button>' +
+          '<a class="navbar-brand" href="index.html">Rowan ACM</a>' +
+          '</div>' +
+
+          '<!-- Collect the nav links, forms, and other content for toggling -->' +
+          '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' +
+          '<ul class="nav navbar-nav navbar-right">' +
+          '<li>' +
+            '<a href="index.html">Home</a>' +
+          '</li>' +
+          '<li>' +
+          '<a href="login.html">Login</a>' +
+          '</li>' +
+          '<li>' +
+          '<a href="register.html">Register</a>' +
+          '</li>' +
+          '</ul>' +
+          '</div>' +
+          '<!-- /.navbar-collapse -->' +
+          '</div>' +
+          '<!-- /.container -->');*/
     }
   }, function(error) {
     console.log(error);
