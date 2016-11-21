e9dc3a892d7072b999b4114b677869ce6e8440ad
tylercarberry <tylercarberry@gmail.com>
2016-11-21 12:16:51 -0500

    Add settings activity

--- app/src/main/java/org/rowanacm/android/MainActivity.java
+++ app/src/main/java/org/rowanacm/android/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.us.acm.R;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -35,10 +38,48 @@ public class MainActivity extends AppCompatActivity {
         initWebView();
     }
 
+    /**
+     * Create the menu bar on the top of Main Activity
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    /**
+     * Gets called when an item in the menu bar is tapped
+     * @param item The menu item that was selected
+     * @return true to finish processing of selection, or false to perform the normal menu handling
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                switchActivity(SettingsActivity.class);
+                return true;
+            //case R.id.help:
+            //    startActivity(new Intent(this, Help.class));
+            //    return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        //FirebaseMessaging.getInstance().subscribeToTopic("test_notifications");
+    }
+
     @OnClick(R.id.attendance_button)
     protected void switchToAttendanceActivity() {
         switchActivity(AttendanceActivity.class);
     }
+
+
 
     private void initWebView() {
         webView = (WebView) findViewById(R.id.webview);
