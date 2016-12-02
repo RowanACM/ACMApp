953a788d90485400d2677e257c5e505aa956e850
Johnathan Saunders <saundersokj@gmail.com>
2016-12-02 01:17:49 -0500

    added setttings stuff i left out

--- app/src/main/java/org/rowanacm/android/MainTabActivity.java
+++ app/src/main/java/org/rowanacm/android/MainTabActivity.java
@@ -1,5 +1,6 @@
 package org.rowanacm.android;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import android.os.Bundle;
 import android.us.acm.R;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,26 +66,30 @@ public class MainTabActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * Create the menu bar on the top of Main Activity
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_tab, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.settings:
+                switchActivity(SettingsActivity.class);
+                return true;
+            //case R.id.help:
+            //    startActivity(new Intent(this, Help.class));
+            //    return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -162,4 +168,9 @@ public class MainTabActivity extends AppCompatActivity {
             return null;
         }
     }
+    private void switchActivity(Class newActivity) {
+        Intent intent = new Intent(this, newActivity);
+        startActivity(intent);
+    }
+
 }
