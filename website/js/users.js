a2001867d7271a993b99e1671dc070fe3476b5fe
chris pryer <christophpryer@gmail.com>
2016-12-13 14:55:24 -0500

    sign in and sign out + navbar work

--- website/js/users.js
+++ website/js/users.js
@@ -24,19 +24,26 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 ui.start('#firebaseui-auth-container', uiConfig);
 
 initApp = function() {
+
+  //this should become more dynamic, with more intuitive control/interfacing.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      window.location = "index.html";
-        // User is signed in.
+
+
+        // User is signed in. Below are properties of the user.
         var displayName = user.displayName;
         var email = user.email;
         var emailVerified = user.emailVerified;
         var photoURL = user.photoURL;
         var uid = user.uid;
         var providerData = user.providerData;
+
+        // this is for more user/site control
         user.getToken().then(function(accessToken) {
           document.getElementById('sign-in-status').textContent = 'Signed in';
           document.getElementById('sign-in').textContent = 'Sign out';
+
+          // keep if needed
           document.getElementById('account-details').textContent = JSON.stringify({
 
             displayName: displayName,
@@ -48,6 +55,56 @@ initApp = function() {
             providerData: providerData
           }, null, '  ');
         });
+
+        // update navbar
+        var navBar = document.getElementById("navContainer");
+        navBar.innerHTML =
+        '<div class="navbar-header page-scroll">' +
+            '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">' +
+                '<span class="sr-only">Toggle navigation</span>' +
+                'Menu <i class="fa fa-bars"></i>' +
+            '</button>' +
+            '<a class="navbar-brand" href="index.html">Dashboard</a>' +
+        '</div>' +
+        '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">' +
+        '<ul class="nav navbar-nav navbar-right">' +
+            '<li>' +
+                '<a href="index.html">Home</a>' +
+            '</li>' +
+            '<li>' +
+                '<a href="committees.html">Committees</a>' +
+            '</li>' +
+            '<li>' +
+                '<a href="index.html" onclick="signOut();">Sign out</a>' +
+            '</li>' +
+        '</ul>'
+        '</div>';
+    } else {
+      var navBar = document.getElementById("navContainer");
+      navBar.innerHTML =
+      '<div class="navbar-header page-scroll">' +
+          '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">' +
+              '<span class="sr-only">Toggle navigation</span>' +
+              'Menu <i class="fa fa-bars"></i>' +
+          '</button>' +
+          '<a class="navbar-brand" href="register.html">Become A Member</a>' +
+      '</div>' +
+      '<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">' +
+          '<ul class="nav navbar-nav navbar-right">' +
+              '<li>' +
+                  '<a href="index.html">Home</a>' +
+              '</li>' +
+              '<li>' +
+                '<a href="committees.html">Committees</a>' +
+              '</li>' +
+              '<li>' +
+                  '<a href="login.html">Login</a>' +
+              '</li>' +
+              '<li>' +
+                  '<a href="register.html">Register</a>' +
+              '</li>' +
+          '</ul>' +
+      '</div>';
     }
   }, function(error) {
     console.log(error);
@@ -94,3 +151,8 @@ var showError = function(message) {
 window.addEventListener('load', function() {
   initApp()
 });
+
+var signOut = function() {
+  firebase.auth().signOut();
+  location.reload();
+};
