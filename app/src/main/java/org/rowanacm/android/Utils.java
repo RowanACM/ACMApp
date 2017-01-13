3babb8c52b00f45949f597f523151fdc7e0faa5b
Tyler Carberry <TylerCarberry@users.noreply.github.com>
2017-01-12 20:39:37 -0500

    Add google sign in
    Only displays your email in a toast

---
+++ app/src/main/java/org/rowanacm/android/Utils.java
@@ -0,0 +1,27 @@
+package org.rowanacm.android;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
+
+public class Utils {
+
+    /**
+     * Open a url in a Chrome Custom Tab
+     * @param url the url to open
+     */
+    public static void openUrl(Context context, String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(context, Uri.parse(url));
+    }
+
+    public static String readFromSharedPreferenceString(Context context, String key) {
+        SharedPreferences prefs = context.getSharedPreferences(
+                "org.rowanacm.android", Context.MODE_PRIVATE);
+
+        return prefs.getString(key, ""); // the default value is an empty string
+    }
+}
--- app/src/main/java/org/rowanacm/android/attendance/AttendanceActivity.java
+++ app/src/main/java/org/rowanacm/android/attendance/AttendanceActivity.java
@@ -3,10 +3,9 @@ package org.rowanacm.android.attendance;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.support.customtabs.CustomTabsIntent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -14,26 +13,49 @@ import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.us.acm.R;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import org.rowanacm.android.Utils;
 import org.rowanacm.android.nfc.NfcManager;
 
 import java.io.IOException;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Sign a member in to the meeting
  */
-public class AttendanceActivity extends AppCompatActivity {
+public class AttendanceActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "AttendanceActivity";
 
     private final static String defaultAttendanceFormUrl = "https://docs.google.com/forms/d/e/1FAIpQLScgL5EttHTj4HblJrkIoSRo560gseCQFoypADL7qEd5UdJlnA/viewform";
+    private static final int RC_SIGN_IN = 4;
 
     // Handles reading and writing from NFC
     NfcManager nfcManager;
+    private GoogleApiClient mGoogleApiClient;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +63,7 @@ public class AttendanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attendance);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         // Create the NFC Manager
         createNfcManager();
@@ -55,21 +78,8 @@ public class AttendanceActivity extends AppCompatActivity {
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        Button attendanceButton = (Button) findViewById(R.id.attendance_button);
-        attendanceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                User user = getUser();
-
-                String attendanceUrl = generateAttendanceUrl(user);
-
-                openUrl(attendanceUrl);
-            }
-        });
-
         EditText nameEditText = (EditText) findViewById(R.id.name_exit_text);
-        nameEditText.setText(readFromSharedPreferenceString("name"));
+        nameEditText.setText(Utils.readFromSharedPreferenceString(this, "name"));
         nameEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
@@ -88,7 +98,7 @@ public class AttendanceActivity extends AppCompatActivity {
 
 
         EditText emailEditText = (EditText) findViewById(R.id.email_edit_text);
-        emailEditText.setText(readFromSharedPreferenceString("email"));
+        emailEditText.setText(Utils.readFromSharedPreferenceString(this, "email"));
         emailEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
@@ -107,6 +117,96 @@ public class AttendanceActivity extends AppCompatActivity {
             }
         });
 
+        mAuth = FirebaseAuth.getInstance();
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+    }
+
+    @OnClick(R.id.attendance_button)
+    public void signInAttendance() {
+        User user = getUser();
+        String attendanceUrl = generateAttendanceUrl(user);
+        Utils.openUrl(AttendanceActivity.this, attendanceUrl);
+    }
+
+    @OnClick(R.id.sign_in_google_button)
+    public void signInGoogle() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Log.d(TAG, "onActivityResult: rcsignin");
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                Log.d(TAG, "onActivityResult: success");
+
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                Log.d(TAG, "onActivityResult: failed else");
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Toast.makeText(this, acct.getEmail(), Toast.LENGTH_SHORT).show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(AttendanceActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
     }
 
     private User getUser() {
@@ -118,11 +218,15 @@ public class AttendanceActivity extends AppCompatActivity {
         return user;
     }
 
-
-
     @Override
     public void onNewIntent(Intent intent) {
         nfcManager.onActivityNewIntent(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
@@ -135,6 +239,14 @@ public class AttendanceActivity extends AppCompatActivity {
     protected void onPause() {
         nfcManager.onActivityPause();
         super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     /**
@@ -219,13 +331,6 @@ public class AttendanceActivity extends AppCompatActivity {
         */
     }
 
-    private String readFromSharedPreferenceString(String key) {
-        SharedPreferences prefs = AttendanceActivity.this.getSharedPreferences(
-                "org.rowanacm.android", Context.MODE_PRIVATE);
-
-        return prefs.getString(key, ""); // the default value is an empty string
-    }
-
     private String generateAttendanceUrl(User user) {
         if(!user.isValid())
             return defaultAttendanceFormUrl;
@@ -235,14 +340,8 @@ public class AttendanceActivity extends AppCompatActivity {
         return "https://docs.google.com/forms/d/e/1FAIpQLScgL5EttHTj4HblJrkIoSRo560gseCQFoypADL7qEd5UdJlnA/viewform?entry.319595206=" + formattedName + "&entry.1988864937=" + user.getRowanEmailAddress() + "&entry.1997712893&entry.717459855&entry.405789413&entry.856944836";
     }
 
-    /**
-     * Open a url in a Chrome Custom Tab
-     * @param url the url to open
-     */
-    private void openUrl(String url) {
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.launchUrl(this, Uri.parse(url));
-    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
 }
