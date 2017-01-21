8e4073a40db6d351dfce3b98feb59a17a5290b65
Johnathan Saunders <saundersokj@gmail.com>
2017-01-21 02:02:49 -0500

    init build of ios app, added firebase and firebase database

--- ACM/ViewController.swift
+++ ACM/ViewController.swift
@@ -5,13 +5,50 @@
 //  Created by Johnathan Saunders on 1/20/17.
 //  Copyright © 2017 Johnathan Saunders. All rights reserved.
 //
-
+import Foundation
 import UIKit
+class AnnouncementTableViewCell: UITableViewCell {
+    @IBOutlet weak var committee: UILabel!
+    @IBOutlet weak var body: UILabel!
+    
+}
+class ViewController: BaseViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    
+    struct Announcement {
+        var author : String = ""
+        var committee : String = ""
+        var date : String = "";
+        var text : String = ""
+        var timestamp : Int = 0
+        var title : String = ""
+    }
+    var items: Array<Announcement> = Array<Announcement>();
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        self.title = "ACM";
+       let announcementRef =  BaseViewController.ref.child("announcements")
+        // Listen for new comments in the Firebase database
+        announcementRef.observe(.childAdded, with: { (snapshot) -> Void in
+            let value = snapshot.value as? NSDictionary
+
+            var announcement = Announcement()
+                
+            announcement.author = value?["author"] as? String ?? ""
+            announcement.committee = value?["committee"] as? String ?? ""
+            announcement.date = value?["date"] as? String ?? ""
+            announcement.text = value?["text"] as? String ?? ""
+            announcement.timestamp = value?["timestamp"] as? Int ?? 0
+            announcement.title = value?["title"] as? String ?? ""
+            self.items.append(announcement)
+            self.tableView.reloadData()
+            
+            print(announcement)
+        })
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +56,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:AnnouncementTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "AnnouncementTableViewCell")! as! AnnouncementTableViewCell
+        var announcement = self.items[indexPath.row]
+        cell.committee.text = announcement.committee 
+        cell.body.text = announcement.text
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
 
 
 }
---
+++ Pods/Target Support Files/Pods-ACMUITests/Pods-ACMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ACMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ACMUITests
+@end
