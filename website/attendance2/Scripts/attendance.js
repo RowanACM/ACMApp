6e7d677e5410093334b624b2fbc750bec4c0f904
tylercarberry <tylercarberry@gmail.com>
2017-01-22 22:53:34 -0500

    Improve attendance

--- website/attendance2/Scripts/attendance.js
+++ website/attendance2/Scripts/attendance.js
@@ -1,8 +1,5 @@
 // ACM
 
-var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1cFTIl6Xy8vsSneUq8DOwUpJmGcO2atZFndHlJvli89Q/pubhtml';
-
-var theData = null;
 var signed_in_count = 0;
 var new_member_count = 0;
 
@@ -12,60 +9,54 @@ var attendanceEnabled = false;
 var signedInGoogle = false;
 var signedInMeeting = false;
 
+var meetingSignedInRef;
+
 
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyD52Hpo4RJkxLhHPcRRw9HXd-thHVYD_O0",
-authDomain: "rowan-acm.firebaseapp.com",
-databaseURL: "https://rowan-acm.firebaseio.com",
-storageBucket: "rowan-acm.appspot.com",
-messagingSenderId: "536754237521"
+	apiKey: "AIzaSyD52Hpo4RJkxLhHPcRRw9HXd-thHVYD_O0",
+	authDomain: "rowan-acm.firebaseapp.com",
+	databaseURL: "https://rowan-acm.firebaseio.com",
+	storageBucket: "rowan-acm.appspot.com",
+	messagingSenderId: "536754237521"
 };
 firebase.initializeApp(config);
 
-function updateSignInCount() {
-    document.getElementById("first_meeting").innerHTML = "Number of new members: " + new_member_count;
-    document.getElementById("signed_in").innerHTML = "How many people signed into this meeting: " + signed_in_count;
-}
 
-function firebaseAttendanceSetup() {
-	var attendanceRef = firebase.database().ref('attendance');
-	attendanceRef.on('value', function(snapshot) {
-  		currentMeeting = snapshot.child("current").val();
-  		attendanceEnabled = snapshot.val();
-  		
-  		if(signedInGoogle) {
-  			document.getElementById("attendance").innerHTML = "Sign in to the meeting";
-  			if(signedInGoogle) {
-    			showSignInButton();
-    		}
-    		
-    		determineIfSignedInMeeting();
-  		}
-  		else {
-  			document.getElementById("attendance").innerHTML = "Attendance is disabled";
-  		}
-  		
-  		new_member_count = snapshot.child(currentMeeting).child("new_member_count").val();
-  		signed_in_count = snapshot.child(currentMeeting).child("signed_in_count").val();
-  		updateSignInCount();
-	});
+var attendanceRef = firebase.database().ref('attendance');
+attendanceRef.on('value', function(snapshot) {
+	currentMeeting = snapshot.child("current").val();
+	attendanceEnabled = snapshot.child("enabled").val();
+	new_member_count = snapshot.child(currentMeeting).child("new_member_count").val();
+	signed_in_count = snapshot.child(currentMeeting).child("signed_in_count").val();
 	
-	var provider = new firebase.auth.GoogleAuthProvider();
-	provider.addScope('https://www.googleapis.com/auth/plus.login');
+	if(attendanceEnabled) {
+		if(signedInGoogle) {
+			changeViewsSignedInGoogle();
+		}
+		
+		determineIfSignedInMeeting();
+	}
+	else {
+		document.getElementById("attendance").innerHTML = "Attendance is disabled";
+		document.getElementById("meeting_button").style.visibility = "hidden";
+	}
+	
+	
+	updateSignInViews();
+});
 
-}
 
-firebaseAttendanceSetup()
+
 
 firebase.auth().onAuthStateChanged(function(user) {
-	console.log("ON AUTH STATE CHANGED " + user);
+	console.log("ON AUTH STATE CHANGED " + user + " SIGNED IN: " + (user != null));
 	 
   	if (user) {
   		// User is signed in.
 		signedInGoogle = true;
 		if(attendanceEnabled && !signedInMeeting) {
-			showSignInButton();
+			changeViewsSignedInGoogle()
 		}
 	
 		if(currentMeeting != null) {	
@@ -74,61 +65,48 @@ firebase.auth().onAuthStateChanged(function(user) {
 	} else {
 		// No user is signed in.
 		signedInGoogle = false;
+		document.getElementById("meeting_button").style.visibility = "hidden";
+		document.getElementById("sign_out").style.visibility = "hidden";
+		document.getElementById("google_sign_in").style.visibility = "visible";
+		document.getElementById("attendance").innerHTML = "Sign in to your google account (Rowan Email Address)";
 	}
 });
 
-var already_listening = false;
+
 function determineIfSignedInMeeting() {
-	if(already_listening) return; else already_listening = true;
-	var uid = firebase.auth().currentUser.uid;
-	var meetingSignedInRef = firebase.database().ref('attendance/' + currentMeeting + "/" + uid);
-	meetingSignedInRef.on('value', function(snapshot) {
-		if(snapshot.val() != null && snapshot.child("uid").val() === uid) { // If signed in
-			console.log("ALREADY SIGNED IN");
-			signedInToMeeting();
-		}
-	});  
+	if(firebase.auth().currentUser != null) {
+		var uid = firebase.auth().currentUser.uid;
+		meetingSignedInRef = firebase.database().ref('attendance/' + currentMeeting + "/" + uid);
+		meetingSignedInRef.on('value', function(snapshot) {
+			if(snapshot.val() != null && snapshot.child("uid").val() === uid) { // If signed in
+				console.log("ALREADY SIGNED IN");
+				signedInToMeeting();
+			}
+		}); 
+	} 
 }
 
-function googleSignIn() {
-	firebase.auth().signInWithPopup(provider).then(function(result) {
-	  // This gives you a Google Access Token. You can use it to access the Google API.
-	  var token = result.credential.accessToken;
-	  // The signed-in user info.
-	  var user = result.user;
-	  // ...
-	}).catch(function(error) {
-	  // Handle Errors here.
-	  var errorCode = error.code;
-	  var errorMessage = error.message;
-	  // The email of the user's account used.
-	  var email = error.email;
-	  // The firebase.auth.AuthCredential type that was used.
-	  var credential = error.credential;
-	  // ...
-	});
-}
-
-function showSignInButton() {
-	document.getElementById("meeting_button").style.visibility = "visible";
-}
-
-function hideSignInButton() {
+function signingInToMeeting() {
+	console.log("SIGNING IN TO MEETING");
 	document.getElementById("meeting_button").style.visibility = "hidden";
+	document.getElementById("attendance").innerHTML = "Signing in to the meeting...";
 }
+
 
 function signedInToMeeting() {
 	console.log("SIGNED IN TO MEETING");
 
 	signedInMeeting = true;
-	hideSignInButton();
+	document.getElementById("meeting_button").style.visibility = "hidden";
 	document.getElementById("attendance").innerHTML = "You signed in to the meeting ✓";
 }
-
 
 function submitAttendance() {
 	var user = firebase.auth().currentUser;
 	if (user != null) {
+		console.log("SIGNING IN TO MEETING");
+		signingInToMeeting();
+	
 		$.get({
 		  url: "https://2dvdaw7sq1.execute-api.us-east-1.amazonaws.com/prod/attendance",
 		  type: "get", //send it through get method
@@ -157,15 +135,18 @@ function submitAttendance() {
         			break;
         		case 210:
         			// Invalid input
-        			alert("Invalid input");
+        			document.getElementById("meeting_button").style.visibility = "visible";
+					document.getElementById("attendance").innerHTML = "Error signing in. Try again. ERROR MESSAGE: INVALID INPUT";
         			break;
         		case 220:
-        			// Didn't sign in. Unknown error
-        			alert("Unknown error");
+        			// Invalid input
+        			document.getElementById("meeting_button").style.visibility = "visible";
+					document.getElementById("attendance").innerHTML = "Error signing in. Try again. ERROR MESSAGE: UNKNOWN ERROR";
         			break;
     			default:
-    				// Didn't sign in. Unknown error
-    				alert("Unknown error");
+    				// Invalid input
+        			document.getElementById("meeting_button").style.visibility = "visible";
+					document.getElementById("attendance").innerHTML = "Error signing in. Try again. ERROR MESSAGE: UNKNOWN ERROR";
 			}
 		  },
 		  error: function(xhr) {
@@ -173,7 +154,91 @@ function submitAttendance() {
 			alert("Connection Error: " + xhr);
 		  }
 		});
-  	
   	}
-	
+}
+
+
+function updateSignInViews() {
+    document.getElementById("first_meeting").innerHTML = "Number of new members: " + new_member_count;
+    document.getElementById("signed_in").innerHTML = "How many people signed into this meeting: " + signed_in_count;
+}
+
+
+function changeViewsSignedInGoogle() {
+	document.getElementById("meeting_button").style.visibility = "visible";
+	document.getElementById("sign_out").style.visibility = "visible";
+	document.getElementById("google_sign_in").style.visibility = "hidden";
+	document.getElementById("attendance").innerHTML = "Sign in to the meeting";
+}
+
+function googleSignIn() {
+	var provider = new firebase.auth.GoogleAuthProvider();
+	provider.addScope('https://www.googleapis.com/auth/plus.login');
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
+
+function onSignIn(googleUser) {
+  console.log('Google Auth Response', googleUser);
+  // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+  var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+    unsubscribe();
+    // Check if we are already signed-in Firebase with the correct user.
+    if (!isUserEqual(googleUser, firebaseUser)) {
+      // Build Firebase credential with the Google ID token.
+      var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.getAuthResponse().id_token);
+      // Sign in with credential from the Google user.
+      firebase.auth().signInWithCredential(credential).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+    } else {
+      console.log('User already signed-in Firebase.');
+    }
+  });
+}
+
+function isUserEqual(googleUser, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === googleUser.getBasicProfile().getId()) {
+        // We don't need to reauth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Sign out of Firebase and Google
+function firebaseSignOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+    firebase.auth().signOut();
+    signedInMeeting = false;
 }
