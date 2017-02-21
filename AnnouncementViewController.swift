1a9119c28d0e67d13ad1d2ba6064b41d303c0871
tylercarberry <tylercarberry@gmail.com>
2017-02-21 12:51:20 -0500

    Show relative date for annoucemements
    Ex. 2 hours ago, yesterday, 3 weeks ago

--- AnnouncementViewController.swift
+++ AnnouncementViewController.swift
@@ -18,10 +18,52 @@ class AnnouncementViewController : BaseViewController{
         self.title = "Announcement";
 
         committee.text = announcement.committee
-       var date = NSDate(timeIntervalSince1970: TimeInterval(announcement.timestamp))
-        timestamp.text = date.description
+        let date = NSDate(timeIntervalSince1970: TimeInterval(announcement.timestamp))
+        let relativeDate = timeAgoSinceDate(date: date)
+        timestamp.text = relativeDate
         body.text = announcement.text
         print(announcement)
+    }
+    
+    // Get the relative time between a given date and the current time
+    func timeAgoSinceDate(date:NSDate) -> String {
+        let calendar = NSCalendar.current
+        let unitFlags: Set<Calendar.Component> = [.minute, .hour, .day, .weekOfYear, .month, .year, .second]
+        let now = NSDate()
+        let earliest = now.earlierDate(date as Date)
+        let latest = (earliest == now as Date) ? date : now
+        let components = calendar.dateComponents(unitFlags, from: earliest as Date,  to: latest as Date)
+        
+        if (components.year! >= 2) {
+            return "\(components.year!) years ago"
+        } else if (components.year! >= 1){
+            return "1 year ago"
+        } else if (components.month! >= 2) {
+            return "\(components.month!) months ago"
+        } else if (components.month! >= 1){
+            return "1 month ago"
+        } else if (components.weekOfYear! >= 2) {
+            return "\(components.weekOfYear!) weeks ago"
+        } else if (components.weekOfYear! >= 1){
+            return "1 week ago"
+        } else if (components.day! >= 2) {
+            return "\(components.day!) days ago"
+        } else if (components.day! >= 1){
+            return "Yesterday"
+        } else if (components.hour! >= 2) {
+            return "\(components.hour!) hours ago"
+        } else if (components.hour! >= 1){
+            return "1 hour ago"
+        } else if (components.minute! >= 2) {
+            return "\(components.minute!) minutes ago"
+        } else if (components.minute! >= 1){
+            return "1 minute ago"
+        } else if (components.second! >= 3) {
+            return "\(components.second!) seconds ago"
+        } else {
+            return "Just now"
+        }
+        
     }
   
 }
